210dba81b60330c2cffd352f6631d848819aaf3c
Landoftha <58531437+Landoftha@users.noreply.github.com>
2024-01-26 12:25:02 -0300

    feat(visualization): visualization of   Copernicus Soil Water Index data

---
+++ src/visualization/streamlit/pages/CSW_Index.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import streamlit as st
+import os
+
+
+
+st.set_page_config(
+    page_title="Green Area",
+    page_icon="🌱",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={}
+)
+
+
+folder_path = os.path.join(os.path.dirname(__file__), "../../../../data/raw/Dados_Nasa NVDI_e_CSWI/nasa_cswi/cswi_data.csv")
+PERCENT = 100
+
+df = pd.read_csv(folder_path, sep = ';', index_col=0)
+
+df =  (df * PERCENT).astype(str) + '%'              
+
+df.index = [str.upper(index) for index in df.index]
+df.columns = [str.capitalize(columns) for columns in df.columns]
+
+
+st.sidebar.header("Please Filter Here:")
+state = st.sidebar.multiselect(
+  "Select the State",
+  options=df.index.unique(),
+  default=df.index.all() 
+)
+
+df_selection = df.query(
+    "index == @state "
+)
+
+st.title(":bar_chart: Copernicus Soil Water Index")
+st.markdown("##")
+
+# Check if the dataframe is empty:
+if df_selection.empty:
+    st.warning("No data available based on the current filter settings!")
+    st.stop() # This will halt the app from further execution.
+    
+
+st.dataframe(df_selection)    
